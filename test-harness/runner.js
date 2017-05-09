346807e3c8309477aa18c9f5a90c9a4f4f8d6f26
Markus Wolf <markus.wolf@sinnerschrader.com>
2017-05-09 18:03:32 +0200

    chore: build before run harness

--- test-harness/runner.js
+++ test-harness/runner.js
@@ -3,7 +3,7 @@ const { sync: globbySync } = require('globby');
 const { join } = require('path');
 const ts = require('typescript');
 
-const { emit } = require('../dist/src');
+const { emit } = require('..');
 
 module.exports = {
   runner: function(part) {
