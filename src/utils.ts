ebf376efe8e8a4ec52de61275707f9c43a10c3f4
Markus Wolf <mail@markus-wolf.de>
2017-05-23 12:14:45 +0200

    fix: add more whitespace characters

--- src/utils.ts
+++ src/utils.ts
@@ -11,14 +11,14 @@ export function emitStatic(source: string[], text: string, node: ts.Node,
 export function addWhitespace(source: string[], node: ts.Node, context: EmitterContext): void {
   if (context.offset <= node.getFullStart()) {
     const text = context.sourceFile.text.substring(node.getFullStart(), node.end);
-    const leadingWhitespace = text.match(/^(\s+)/);
+    const leadingWhitespace = text.match(/^([ \f\n\r\t\v\u0085\u00A0\u2028\u2029]+)/);
     if (leadingWhitespace) {
       context.offset = node.getFullStart() + leadingWhitespace[1].length;
       source.push(leadingWhitespace[1]);
     }
   } else {
     const text = context.sourceFile.text.substring(context.offset, node.end);
-    const trailingWhitespace = text.match(/^(\s+)/);
+    const trailingWhitespace = text.match(/^([ \f\n\r\t\v\u0085\u00A0\u2028\u2029]+)/);
     if (trailingWhitespace) {
       context.offset = context.offset + trailingWhitespace[1].length;
       source.push(trailingWhitespace[1]);
