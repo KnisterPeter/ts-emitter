5e18764757589972a0b0e804a29c8aeea71764c9
Markus Wolf <mail@markus-wolf.de>
2017-05-23 11:19:28 +0200

    feat: add support for encodings (e.g. utf16)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,44 @@
+import * as fs from 'fs';
 import * as ts from 'typescript';
 
 import { emit as internalEmit } from './emitter';
 
-export function emit(node: ts.SourceFile): string {
-  return internalEmit(node, {
-    sourceFile: node,
+export function emit(path: string, encoding: string): string;
+export function emit(node: ts.SourceFile): string;
+export function emit(pathOrNode: string|ts.SourceFile, encoding?: string): string {
+  const sourceFile = typeof pathOrNode === 'string'
+    ? readFile(pathOrNode, encoding || 'utf8')
+    : pathOrNode;
+  return internalEmit(sourceFile, {
+    sourceFile,
     offset: 0
   });
+}
+
+function readFile(filepath: string, encoding: string): ts.SourceFile {
+  const source = fs.readFileSync(filepath).toString(encoding);
+  const options = Object.assign(
+    {},
+    ts.getDefaultCompilerOptions(),
+    {
+      experimentalDecorators: true,
+      jsx: ts.JsxEmit.Preserve,
+      noEmit: true,
+      noResolve: true,
+      preserveConstEnums: true,
+      removeComments: false,
+      target: ts.ScriptTarget.Latest
+    }
+  );
+  const host = Object.assign(
+    {},
+    ts.createCompilerHost(options, true),
+    {
+      getSourceFile(fileName: string, languageVersion: ts.ScriptTarget): ts.SourceFile {
+        return ts.createSourceFile(fileName, source, languageVersion, true);
+      }
+    }
+  );
+  const program = ts.createProgram([filepath], options, host);
+  return program.getSourceFile(filepath);
 }
--- test-harness/runner.js
+++ test-harness/runner.js
@@ -3,8 +3,19 @@ const { sync: globbySync } = require('globby');
 const { join } = require('path');
 const ts = require('typescript');
 const blacklisted = require('./blacklisted-tests.json');
+const iconv = require('iconv-lite');
 
 const { emit } = require('..');
+
+function getBOM(input) {
+  const _0 = input[0];
+  const _1 = input[1];
+  const _2 = input[2];
+  return _0 === 0xFE && _1 === 0xFF ? 'utf16be'
+    : _0 === 0xFF && _1 === 0xFE ? 'utf16le'
+    : _0 === 239 && _1 === 187 && _2 === 191 ? 'utf8'
+    : 'utf8';
+}
 
 function getSourceFile(path, source) {
   const options = Object.assign(
@@ -29,7 +40,7 @@ function getSourceFile(path, source) {
       }
     }
   );
-  const name = source.match(/@jsx/) ? 'source.tsx' : 'source.ts';
+  const name = source.toString().match(/@jsx/) ? 'source.tsx' : 'source.ts';
   const program = ts.createProgram([name], options, host);
   return program.getSourceFile(name);
 }
@@ -43,7 +54,9 @@ module.exports = {
     if (paths.length > 0) {
       paths.forEach(path => {
         test(path, () => {
-          const source = readFileSync(path).toString();
+          const buffer = readFileSync(path);
+          const encoding = getBOM(buffer);
+          const source = iconv.decode(buffer, encoding);
           expect(emit(getSourceFile(path, source))).toBe(source);
         });
       });
